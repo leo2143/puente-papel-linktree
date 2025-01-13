336365715786998c8a95bfc09d2c7f26e2294069
Leonardo <orellanaleonardo782@gmail.com>
2025-01-12 22:07:26 -0300

    feat: added new link

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
 
 const socialLinks = [
     {
+      id: "instagram",
       href: "https://www.instagram.com/puentepapel?igsh=bHlzNTc2aWV0Y2t0",
       text: "Instagram",
       img: `/imagenes/instagram-logo.svg`,
       alt: "Instagram Logo",
     },
     {
+      id: "Blog",
       href: "https://comunidadpuentepapel.blogspot.com/",
       text: "Blog",
       img: `/imagenes/blog-writing-svgrepo-com.svg`,
       alt: "Blog Writing Logo",
     },
     {
+      id: "Facebook",
       href: "https://www.facebook.com/share/oWwg7kxJ1GGEq9Fc",
       text: "Facebook",
       img: `/imagenes/facebook-svgrepo-com.svg`,
       alt: "Facebook Logo",
     },
     {
+        id: "whatsapp",
         href: "https://wa.me/c/5491124636219",
         text: "Nuestro Catalogo",
         img: `/imagenes/whatsapp-svgrepo-com.svg`,
         alt: "whatsapp Logo",
       
-    }
+    },
+    {
+      id: "shop",
+      href: "https://puentepapel.empretienda.com.ar/",
+      text: "Tienda",
+      img: `/imagenes/bx-shopping-bag.svg`,
+      alt: "shop Logo",
+    
+  }
   ];
 function createSocialLinks(links) {
     const mainContainer = document.querySelector(".main-container");
@@ -38,6 +50,7 @@ function createSocialLinks(links) {
   
       // Crear el contenedor principal
       const containerSocialMedia = document.createElement("div");
+      containerSocialMedia.id = link.id
       containerSocialMedia.className = "container-social-media";
   
       // Crear el contenedor del logo
